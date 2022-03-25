53b98b7ee79673b02c5f7649c2dfde914ca1e08d
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-03-26 00:40:37 +0600

    FakeData and Header Added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1><span className='title'>AMBIGUOUS</span> CHOICE...</h1>
+            <hr />
+        </div>
+    );
+};
+
+export default Header;
